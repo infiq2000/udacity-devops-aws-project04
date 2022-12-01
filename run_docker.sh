07113513b7ea92bd146b258aa2370e4991043c9a
Ubuntu <ubuntu@ip-172-31-19-127.ec2.internal>
2022-12-01 12:57:15 +0000

    add sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+# Step 1: Build image and add a descriptive tag
+docker build -t ml-training:latest .
+docker tag ml-training:latest datht20fpt/ml_traning:latest
 
-# Step 1:
-# Build image and add a descriptive tag
+# Step 2: List docker images
+docker images ls
 
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
+# Step 3: Run flask app
+docker run -p 8000:80 ml-training:latest
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,16 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
-# This is your Docker ID/path
-# dockerpath=<>
+# Step 1: This is your Docker ID/path
+dockerpath=datht20fpt/ml_traning:latest
 
-# Step 2
-# Run the Docker Hub container with kubernetes
+# Step 2: Run the Docker Hub container with kubernetes
+kubectl create deploy ml-training --image=$dockerpath
 
+# Step 3: List kubernetes pods
+kubectl get pods -n default -o wide
 
-# Step 3:
-# List kubernetes pods
-
-# Step 4:
-# Forward the container port to a host
+# Step 4: Forward the container port to a host
+# kubectl expose pod <pod-name> --port=80 --name=ml-training-svc
+kubectl port-forward datht20fptapp 8000:80
 
